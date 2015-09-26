3117e12aa3d158ac3f45c27c2a64633111f08e56
fermino <fermino.github@gmail.com>
2015-09-26 14:09:13 -0300

    Autoloader fail xD

--- _Loader.php
+++ _Loader.php
@@ -1,7 +1,7 @@
 <?php
 	function LoadLib($Lib)
 	{
-		require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . $Class . '.php';
+		require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . $Lib . '.php';
 	}
 
 	spl_autoload_register('LoadLib');
